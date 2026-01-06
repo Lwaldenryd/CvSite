e248e21f177cf702ebc09d48dd1c94f9e6c09d50
Mwettermark <wettermark03@gmail.com>
2026-01-06 14:53:35 +0100

    edit knappen fixad

---
+++ CvSite.Web/Models/EditProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CvSite.Web.Models
+{
+    public class EditProfileViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Display(Name = "Private profile")]
+        public bool IsPrivate { get; set; }
+
+        public IFormFile ProfileImage { get; set; }
+    }
+}
