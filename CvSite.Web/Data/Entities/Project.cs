c1d1bde102370d15ad1f4d2c2b87d50801c1f539
Lwaldenryd <lucas.waldenryd@hotmail.se>
2026-01-06 15:45:15 +0100

    hora

---
+++ CvSite.Web/Data/Entities/Project.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CvSite.Web.Data.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        
+        public string OwnerId { get; set; } = null!;
+        public virtual ApplicationUser Owner { get; set; } = null!;
+
+        
+        public virtual ICollection<ProjectMember> Members { get; set; } = new List<ProjectMember>();
+    }
+}
---
+++ CvSite.Web/Data/Entities/ProjectMember.cs
@@ -0,0 +1,11 @@
+﻿namespace CvSite.Web.Data.Entities
+{
+    public class ProjectMember
+    {
+        public int ProjectId { get; set; }
+        public virtual Project Project { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+        public virtual ApplicationUser User { get; set; } = null!;
+    }
+}
